7da9da17968dfb3e17586ac886c0b835a02b2ea9
murdex5 <fkadupiige@gmail.com>
2022-10-14 12:20:58 +0200

    python

--- script.py
+++ script.py
@@ -3,3 +3,18 @@ y = 88
 sum = x + y
 
 print(sum)
+
+name = input('enter your name: ')
+
+
+print(f'Hello malsha {name}')
+
+
+age = input('Eneter your age: ')
+
+if age != int:
+    print('please enter a number')
+
+age_int = int(age)
+
+print(age)
